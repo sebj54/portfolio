6f7d4eca9464f90027e9639e9c5aa8abd4f97b99
Sébastien JEAN <contact@sebjean.com>
2020-09-15 19:38:22 +0200

    💄 Use cards in lists

--- src/components/CreationsList.vue
+++ src/components/CreationsList.vue
@@ -4,29 +4,50 @@
             v-for="(creation, index) in list"
             :key="`creations-list-item-${index}`"
             :to="`creations/${creation.node.slug}`"
-            class="creations-list-item"
+            class="creations-list-item d-block my-4"
         >
-            <g-image
-                class="creations-list-item-coverImage"
-                :src="creation.node.coverImage"
-                alt=""
-            />
-            <h2 class="creations-list-item-title">
-                {{ creation.node.title }}
-            </h2>
-            <p class="creations-list-item-date">
-                {{ creation.node.date }}
-            </p>
-            <p class="creations-list-item-excerpt">
-                {{ creation.node.excerpt }}
-            </p>
+            <b-card
+                no-body
+            >
+                <g-image
+                    class="creations-list-item-coverImage card-img"
+                    :src="creation.node.coverImage"
+                    alt=""
+                />
+
+                <b-card-body>
+                    <b-card-title class="creations-list-item-title">
+                        {{ creation.node.title }}
+                    </b-card-title>
+
+                    <b-card-text class="creations-list-item-date">
+                        {{ creation.node.date }}
+                    </b-card-text>
+
+                    <b-card-text class="creations-list-item-excerpt">
+                        {{ creation.node.excerpt }}
+                    </b-card-text>
+                </b-card-body>
+            </b-card>
         </g-link>
     </div>
 </template>
 
 <script>
+import {
+    BCard,
+    BCardBody,
+    BCardTitle,
+    BCardText,
+} from 'bootstrap-vue'
 
 export default {
+    components: {
+        BCard,
+        BCardBody,
+        BCardTitle,
+        BCardText,
+    },
     props: {
         list: {
             type: Array,
--- src/components/TechnologiesList.vue
+++ src/components/TechnologiesList.vue
@@ -4,23 +4,40 @@
             v-for="(tech, index) in list"
             :key="`technologies-list-item-${index}`"
             :to="`technologies/${tech.node.slug}`"
-            class="about-technologie-item"
+            class="about-technologie-item d-block my-4"
         >
-            <g-image
-                class="technologies-list-item-coverImage"
-                :src="tech.node.coverImage"
-                alt=""
-            />
-            <h2 class="technologies-list-item-title">
-                {{ tech.node.title }}
-            </h2>
+            <b-card
+                no-body
+            >
+                <g-image
+                    class="technologies-list-item-coverImage card-img"
+                    :src="tech.node.coverImage"
+                    alt=""
+                />
+
+                <b-card-body>
+                    <b-card-title class="technologies-list-item-title">
+                        {{ tech.node.title }}
+                    </b-card-title>
+                </b-card-body>
+            </b-card>
         </g-link>
     </div>
 </template>
 
 <script>
+import {
+    BCard,
+    BCardBody,
+    BCardTitle,
+} from 'bootstrap-vue'
 
 export default {
+    components: {
+        BCard,
+        BCardBody,
+        BCardTitle,
+    },
     props: {
         list: {
             type: Array,
