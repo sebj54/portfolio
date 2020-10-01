bfc7bc3d0361b124234c33767e5dfd6bcbbb7fdb
cbrasseur <cdric.brasseur@gmail.com>
2020-10-01 15:47:17 +0200

    🗑 Remove useless code from tests

--- tests/helpers/mountWithI18n.js
+++ tests/helpers/mountWithI18n.js
@@ -1,7 +1,5 @@
 import { createLocalVue, mount } from '@vue/test-utils'
 import VueI18n from 'vue-i18n'
-import VueRouter from 'vue-router'
-import BootstrapVue from 'bootstrap-vue'
 import gridsomeConfig from '~/../gridsome.config'
 
 export default function mountWithI18n(component, options) {
@@ -12,11 +10,8 @@ export default function mountWithI18n(component, options) {
 
     const localVue = createLocalVue()
     localVue.use(VueI18n)
-    localVue.use(VueRouter)
-    localVue.use(BootstrapVue)
 
     const i18n = new VueI18n(pluginOptions)
-    const router = new VueRouter()
 
     if (!options) {
         options = {}
@@ -24,7 +19,6 @@ export default function mountWithI18n(component, options) {
 
     options.localVue = localVue
     options.i18n = i18n
-    options.router = router
 
     return mount(component, options)
 }
